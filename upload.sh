14babf22415fad572d28f86070034c4c39ee5395
EricDeamn <53068730+EricGao-Byte@users.noreply.github.com>
2021-07-06 15:12:46 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # rclone qbittorrent下载完成自动上传onedrive和google drive
+chat_id="" #用户id，通过 @userinfobot 获取
+bot_api="" #自己申请的bot的API
 file=$1
 if [ -d "$file" ]; then
     /usr/bin/rclone copy "$1" gd0:/rclone/"$2" --log-file=/home/ericdeamn/rclone.log 
@@ -16,3 +18,5 @@ elif [ -f "$file" ]; then
     #/usr/bin/rclone copy "$1" od1:/rclone/ --log-file=/home/ericdeamn/rclone.log
     # /usr/bin/rclone copy "$1" od2:/rclone/ --log-file=/home/ericdeamn/rclone.log
 fi
+
+curl -g -i -X GET "https://api.telegram.org/bot${bot_api}/sendMessage?chat_id=${chat_id}&text=种子名称：$2已上传到gd"
